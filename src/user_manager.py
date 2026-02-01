a18e539f8be57066995952b65ccd73c46305af19
Paha605 <Pokunev54@gmail.com>
2026-02-01 09:05:10 +0000

    final version

--- src/user_manager.py
+++ src/user_manager.py
@@ -3,7 +3,7 @@ from src.config import ADMIN, SUPERADMIN
 
 # Преобразуем строки в список целых чисел
 admin_list = [int(x.strip()) for x in ADMIN.split(",") if x.strip()]
-superadmin_id = int(SUPERADMIN.strip())
+superadmin_id = int(SUPERADMIN)
 
 def is_admin(user_id):
     return user_id in admin_list
